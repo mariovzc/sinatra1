c6988771dc0ca513a35549ced248f77033ae23f8
¨Mario <mario.vizcainos@gmail.com>
2017-07-21 19:49:42 -0500

    m

--- solution.rb
+++ solution.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 get '/?:name?' do
-  name = params[:name].nil? ? 'desconocido' : params[:name]
-  "<h1> #{name} </h1>"
+  name = params[:name].nil? ? 'desconocido' : params[:name].split("=")[1]
+    "<h1> #{name} </h1>"
 end
